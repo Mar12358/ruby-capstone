0b3094b43f4d793b372d45658ad43c54b6bc3419
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-08-07 17:03:11 -0300

    Refactor label tests

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -4,7 +4,7 @@ require_relative '../item.rb'
 describe Label do
   let(:label) { Label.new('Important', 'Red') }
   let(:item) { Item.new('20/12/2020') }
-  it 'Create an instance' do
+  it 'Create an instance of Label' do
     expect(label).to be_an_instance_of(Label)
     expect(label.title).to eq('Important')
     expect(label.color).to eq('Red')
@@ -12,5 +12,10 @@ describe Label do
   it 'Add an item to the items instance variable' do
     label.add_item(item)
     expect(label.items.length).to eq(1)
+  end
+
+  it 'Use the setter method of the item to set the item label' do
+    label.add_item(item)
+    expect(item.label).to eq(label)
   end
 end
