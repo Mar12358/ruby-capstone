a8f227c5b3d6350c5573c43550e30b341c0ca9c7
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-08-08 14:46:26 -0300

    Fix game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,13 +4,13 @@ require 'rspec'
 RSpec.describe Game do
   describe '#can_be_archieved?' do
     it 'returns true if the game can be archieved' do
-      game = Game.new('Game Title', '2000-01-01', true, '2010-01-01')
+      game = Game.new('2000-01-01', true, '2010-01-01')
       expect(game.can_be_archieved?).to be(true)
     end
 
     it 'returns false if the game cannot be archieved' do
       # test  of case where the game cannot be archived
-      game = Game.new('Game Title', '2021-01-01', true, '2023-01-01')
+      game = Game.new('2021-01-01', true, '2023-01-01')
       expect(game.can_be_archieved?).to be(false)
     end
   end
